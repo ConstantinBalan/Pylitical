bcf985acfa57a6416d601e0cadb83d52e1767f7c
Constantin Balan <35816870+ConstantinBalan@users.noreply.github.com>
2024-04-10 01:35:43 -0400

    Adding url get and some logic for parsing bills webpage

--- BillScraper.py
+++ BillScraper.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+import requests
 import ChatSummarize
 
 def billsByDate(dateRange):
@@ -10,6 +11,31 @@ def billsByCategory(categoryName):
 def main():
     print('Placeholder')
 
+def tempFuncForGettingBills(parsableHTML):
+    soup = BeautifulSoup(parsableHTML, 'html.parser')
+    titleOfBillStates = {}
+    titleNames = ['Introduced', 'Passed by Chamber', 'Enrolled', 'Adopted']
+    for name in titleNames:
+        titleElement = soup.find('h3', text = name)
+
+        if titleElement:
+            tableElement = titleElement.find_next_sibling()
+            titleOfBillStates[name] = tableElement
+        else:
+            titleOfBillStates[name] = None
+    print(titleOfBillStates)
+
+def getWebpageContents(url):
+    response = requests.get(url)
+
+    if response.status_code == 200:
+       html_content = response.text
+       print(html_content)
+    else:
+        print(f"Failed to fetch HTML from {url}. Status code: {response.status_code}")
+    return html_content
 
 if __name__ == "__main__":
-    main()
+    url = "https://legislature.mi.gov/Bills/DailyReport?dateFrom=2024-03-29&dateTo=2024-04-09"
+    html_object = getWebpageContents(url)
+    tempFuncForGettingBills(html_object)
